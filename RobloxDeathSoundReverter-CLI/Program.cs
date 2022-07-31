ea06c956c7dfefffaf6b23b2fd966cc1ba3c53b0
jluims <devbrxen@gmail.com>
2022-07-30 20:29:20 -0400

    add cli program

---
+++ RobloxDeathSoundReverter-CLI/Program.cs
@@ -0,0 +1,51 @@
+﻿using CommandLine;
+
+namespace RobloxDeathSoundReverter_CLI
+{
+    internal class Program
+    {
+        public class Options
+        {
+            [Option("mode", Required = true, HelpText = "The ROBLOX death sound type")]
+            public string mode { get; set; } = string.Empty;
+        }
+
+
+        private static async Task<int> Main(string[] args)
+        {
+            ParserResult<Options> parserResult = Parser.Default.ParseArguments<Options>(args);
+            if (parserResult.Tag != ParserResultType.Parsed) return 1;
+
+            Options options = ((Parsed<Options>)parserResult).Value;
+
+            if (options.mode != "new" && options.mode != "old")
+            {
+                Console.WriteLine("Invalid mode (new/old)");
+                return 1;
+            }
+
+            string? robloxDir = RobloxUtils.GetRobloxDirectory();
+
+            if (robloxDir == null)
+            {
+                Console.WriteLine("Failed to find roblox directory");
+                return 1;
+            }
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string baseDir = Path.Join(robloxDir, @"content\sounds\");
+                string oofDir = Path.Join(baseDir, "ouch.ogg");
+
+                string oofUrl = "https://github.com/jluims/RobloxDeathSoundReverter/raw/main/sounds/" + (options.mode == "new" ? "ouch" : "uuhhh") + ".ogg";
+                byte[] oofBytes = await httpClient.GetByteArrayAsync(oofUrl);
+
+                File.WriteAllBytes(oofDir, oofBytes);
+            }
+
+            Console.WriteLine("Death sound succesfully changed");
+
+            return 0;
+        }
+    }
+}
---
+++ RobloxDeathSoundReverter-CLI/RobloxUtils.cs
@@ -0,0 +1,51 @@
+﻿using IWshRuntimeLibrary;
+using System.Security.Cryptography;
+
+namespace RobloxDeathSoundReverter_CLI
+{
+    internal class RobloxUtils
+    {
+
+        private static string GetHash(string file)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = System.IO.File.OpenRead(file))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+                }
+            }
+        }
+
+        public static string? GetRobloxDirectory()
+        {
+            string roamingPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string shortcutPath = Path.Join(roamingPath, @"Microsoft\Windows\Start Menu\Programs\Roblox\Roblox Player.lnk");
+
+            if (!System.IO.File.Exists(shortcutPath))
+            {
+                return null;
+            }
+
+            // Find the shortcut target
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
+            string shortcutTarget = shortcut.TargetPath;
+
+            DirectoryInfo? robloxDirInfo = Directory.GetParent(shortcutTarget);
+
+            if (robloxDirInfo == null)
+            {
+                return null;
+            }
+
+            return robloxDirInfo.FullName;
+        }
+
+        public static bool HasNewOof(string robloxDir)
+        {
+            return GetHash(Path.Join(robloxDir, @"content\sounds\ouch.ogg")) == "c089723e98c07accd3036f11e70afa3b";
+        }
+
+    }
+}
